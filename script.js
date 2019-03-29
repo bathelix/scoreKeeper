6f35b973c1012498d8ff7a974105c7a0ece07fbe
BatHelix <bathelix@gmail.com>
2019-03-29 17:54:16 -0400

    second selector added

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 var p1Button = document.querySelector("#p1");
+var p2Button = document.querySelector("#p2");
+
+var p1Display= document.querySelector('#p1Display');
+var p2Display= document.querySelector('#p2Display');
+
+var p1Score = 0;
+var p2Score = 0;
 
 
 p1Button.addEventListener('click',function(){
-    alert('clicked');
+    p1Score++;
+    console.log(p1Score);
+    p1Display.textContent = p1Score;
+});
+
+p2Button.addEventListener('click',function(){
+    p2Score++;
+    console.log(p1Score);
+    p2Display.textContent = p2Score;
 });
